ceaa97e5bdf78fa9eb676cdba33a93aeefc2a840
Menghao <menghaoyu2002@gmail.com>
2020-12-26 16:19:34 -0500

    UI update

--- typeracer-autotyper/main.py
+++ typeracer-autotyper/main.py
@@ -1,35 +1,34 @@
-"""
-This module runs the autotyper.
-Change the delay to your own preference.
-Make sure all libraries are downloaded
-"""
-from autotyper import AutoTyper
-from ui import UserInterface
-from ahk import AHK
-from typing import Optional
+# """
+# This module runs the autotyper.
+# Change the delay to your own preference.
+# Make sure all libraries are downloaded
+# """
+# from autotyper import AutoTyper
+# from ui import UserInterface
+# from ahk import AHK
+# from typing import Optional
 
-ahk = AHK()
-autotyper = AutoTyper()
+# ahk = AHK()
+# autotyper = AutoTyper()
 
-def type(typer: AutoTyper, delay: Optional[int] = 0.01) -> None:
-    """Type the text on typeracer out"""
-    typer.getImage()
-    typer.readText()
-    typer.type(delay = delay)
+# def type(typer: AutoTyper, delay: Optional[int] = 0.01) -> None:
+#     """Type the text on typeracer out"""
+#     typer.getImage()
+#     typer.readText()
+#     typer.type(delay = delay)
 
 
-#######################################################################
-#  Main Program Loop
-#######################################################################
-keybind = "Pause"  # default keybind to run script
-run = True
-while run:
-    if ahk.key_state(keybind):
-        type(autotyper)
-    # keybind to end program
-    if ahk.key_state("Escape"):
-        run = False
+# #######################################################################
+# #  Main Program Loop
+# #######################################################################
+# keybind = "Pause"  # default keybind to run script
+# run = True
+# while run:
+#     if ahk.key_state(keybind):
+#         type(autotyper)
+#     # keybind to end program
+#     if ahk.key_state("Escape"):
+#         run = False
 
-    
 
 
--- typeracer-autotyper/ui.py
+++ typeracer-autotyper/ui.py
@@ -1,12 +1,53 @@
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
 
+
+
+root = Tk()
+
+# constants
+
+SCREEN_WITDTH = root.winfo_screenwidth()
+SCREEN_HEIGHT = root.winfo_screenheight()
+
+
+class Slider:
+    def __init__(self, max, variable, label) -> None:
+        self.scale = ttk.Scale(root, orient=HORIZONTAL, length=200, from_=0.0, to=max, variable=variable, command=self.update_label)
+        self.label = label
+
+    def update_label(self, val):
+        self.label['text'] = str(round(float(val), 2))
 
 class UserInterface:
     def __init__(self, root) -> None:
         root.title('Typeracer AutoTyper')
+        mainframe = ttk.Frame(root, padding=10)
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
 
-root = Tk()
+        # x label and sliders
+        x_value = StringVar()
+        x_label = ttk.Label(root)
+        x_label.grid(column=0, row=1, sticky='we')
+        x_scale = Slider(SCREEN_WITDTH, x_value, x_label)
+        x_scale.scale.grid(column=0, row=2, sticky='nw')
+
+        # y labels and sliders
+        y_value = StringVar()
+        y_label = ttk.Label(root)
+        y_label.grid(column=0, row=3, sticky='we')
+        y_scale = Slider(SCREEN_HEIGHT, y_value, y_label)
+        y_scale.scale.grid(column=0, row=4, sticky='nw')
+
+        # delay label and sliders
+
+
+
+
+
+
+
 UserInterface(root)
-root.update()
+root.mainloop()
