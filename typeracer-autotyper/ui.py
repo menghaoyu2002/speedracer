736442145f4ce5960b07fe4cb248bf1b0e1200ed
Menghao <menghaoyu2002@gmail.com>
2020-12-26 17:21:30 -0500

    UI Updates

--- typeracer-autotyper/ui.py
+++ typeracer-autotyper/ui.py
@@ -15,7 +15,7 @@ class Slider:
         self.label = label
 
     def update_label(self, val):
-        self.label['text'] = str(round(float(val), 2))
+        self.label['text'] = str(round(float(val), 3))
 
 class UserInterface:
     def __init__(self, root) -> None:
@@ -25,32 +25,49 @@ class UserInterface:
 
 
         # x label and sliders
+        X = ttk.Label(root, text='x value of box position:', font='Verdana', padding=5)
+        X.grid(column=0, row=0, sticky='nw')
         x_value = StringVar()
-        x_label = ttk.Label(root)
-        x_label.grid(column=0, row=1, sticky='we')
+        x_label = ttk.Label(root,text=0.0, font="Verdana", padding=5)
+        x_label.grid(column=1, row=0, sticky='nw')
         x_scale = Slider(SCREEN_WITDTH, x_value, x_label)
-        x_scale.scale.grid(column=0, row=2, sticky='nw')
+        x_scale.scale.grid(column=0, row=1, sticky='nw')
 
         # y labels and sliders
+        Y = ttk.Label(root, text='y value of box position:', font='Verdana', padding=5)
+        Y.grid(column=0, row=4, sticky='nw')
         y_value = StringVar()
-        y_label = ttk.Label(root)
-        y_label.grid(column=0, row=3, sticky='we')
+        y_label = ttk.Label(root, text=0.0, font='Verdana', padding=5)
+        y_label.grid(column=1, row=4, sticky='nw')
         y_scale = Slider(SCREEN_HEIGHT, y_value, y_label)
-        y_scale.scale.grid(column=0, row=4, sticky='nw')
+        y_scale.scale.grid(column=0, row=5, sticky='nw')
 
         # height labels and sliders
+        height = ttk.Label(root, text='The height of the box:', font='Verdana', padding=5)
+        height.grid(column=0, row=6, sticky='nw')
         height_value = StringVar()
-        height_label = ttk.Label(root)
-        height_label.grid(column=0, row=5, sticky='we')
+        height_label = ttk.Label(root, text=0.0, font='Verdana', padding=5)
+        height_label.grid(column=1, row=6, sticky='nw')
         height_scale = Slider(SCREEN_HEIGHT, height_value, height_label)
-        height_scale.scale.grid(column=0, row=6, sticky='nw')
+        height_scale.scale.grid(column=0, row=7, sticky='nw')
+
+        # width labels and sliders
+        width = ttk.Label(root, text='The width of the box:', font='Verdana', padding=5)
+        width.grid(column=0, row=8, sticky='nw')
+        width_value = StringVar()
+        width_label = ttk.Label(root, text=0.0, font='Verdana', padding=5)
+        width_label.grid(column=1, row=8, sticky='nw')
+        width_scale = Slider(SCREEN_HEIGHT, width_value, width_label)
+        width_scale.scale.grid(column=0, row=9, sticky='nw')
 
         # delay label and sliders
+        width = ttk.Label(root, text='The typing delay is:', font='Verdana', padding=5)
+        width.grid(column=0, row=10, sticky='nw')
         delay_value = StringVar()
-        delay_label = ttk.Label(root)
-        delay_label.grid(column=0, row=7, sticky='we')
+        delay_label = ttk.Label(root, text=0.0, font='Verdana', padding=5)
+        delay_label.grid(column=1, row=10, sticky='nw')
         delay_scale = Slider(1.0, delay_value, delay_label)
-        delay_scale.scale.grid(column=0, row=8, sticky='nw')
+        delay_scale.scale.grid(column=0, row=11, sticky='nw')
 
 
 
